ad61d4cf623a7953aa78996e43a3e15d5df6503b
Marvin B <marvin.b7898@gmail.com>
2023-03-15 17:52:26 +0100

    Talents base de donnée

--- src/Command/TalentCommand.php
+++ src/Command/TalentCommand.php
@@ -28,38 +28,56 @@ class TalentCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
 
+       
+        $this->recursiveImport('https://pokeapi.co/api/v2/ability/', $output);
+
+        return Command::SUCCESS;
+    }
+
+    public function recursiveImport($url, $output){
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://pokeapi.co/api/v2/ability/');
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($ch);
         curl_close($ch);
 
         $json = json_decode($response, true);
 
+        //dd($json['next']);
+
         //$output->writeln($response);
 
         foreach ($json['results'] as $talent) {
             $output->writeln($talent['url']);
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $talent['url']);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $response = curl_exec($ch);
-            curl_close($ch);
-            $json = json_decode($response, true);
-            $output->writeln($json['names'][3]['name']);
+            $ch2 = curl_init();
+            curl_setopt($ch2, CURLOPT_URL, $talent['url']);
+            curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($ch2);
+            curl_close($ch2);
+            $json2 = json_decode($response, true);
+            //$output->writeln($json2['names'][3]['name']);
             // Description française
             // On vérifie que la langue est bien française dans le flavor_text_entries et on affiche le texte si on n'a pas déjà affiché le texte
     
-            foreach ($json['flavor_text_entries'] as $text) {
+            foreach ($json2['flavor_text_entries'] as $text) {
                // dump($text['language']['name']);
                 if ($text['language']['name'] === 'fr') {
-                    $output->writeln($text['flavor_text']);
+                    //$output->writeln($text['flavor_text']);
+
+
+                    $talent = new Talent();
+                    $talent->setNom($json2['names'][3]['name']);
+                    $talent->setDescription($text['flavor_text']);
+                    $this->entityManager->persist($talent);
+                    $this->entityManager->flush();
+
+
                     break;
                 }
             }
         }
-
-
-        return Command::SUCCESS;
+        if (isset($json['next'])) {
+            $this->recursiveImport($json['next'], $output);
+        }
     }
 }
